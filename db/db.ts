871e6051851f90f7afcbfd54daa64d629d192fa7
zackworks <qaiserdevs@gmail.com>
2025-08-05 16:55:30 +0300

    removed  neon serverless instance init

--- db/db.ts
+++ db/db.ts
@@ -1,7 +1,4 @@
-import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-
-export const db = drizzle({ client: sql, schema: schema });
+export const db = drizzle(process.env.DATABASE_URL!, { schema: schema });
