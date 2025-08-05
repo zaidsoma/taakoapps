f62d75da280cca836d4587ed4d25317634fbf30a
zackworks <qaiserdevs@gmail.com>
2025-08-05 16:05:45 +0300

    implemented db on db.ts file connection

--- db/db.ts
+++ db/db.ts
@@ -1,4 +1,17 @@
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
-export const db = drizzle(process.env.DB, { schema });
+export const runtime = "edge";
+
+function initDbConnection() {
+  if (process.env.NODE_ENV === "development") {
+    const { env } = getRequestContext();
+
+    return drizzle(env.DB, { schema });
+  }
+
+  return drizzle(process.env.DB as unknown as D1Database, { schema });
+}
+
+export const db = initDbConnection();
