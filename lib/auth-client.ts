879835573f29dc6e2d5f972960c2088334e8bc0e
zackworks <qaiserdevs@gmail.com>
2025-08-05 19:38:20 +0300

    changed domain for auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
 });
 
 export const { signIn, signUp, useSession } = createAuthClient();
